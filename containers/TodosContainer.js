e7732c11d36b9c986f45412b6c2ca25867aa021a
p-alex <alexey9021@gmail.com>
2021-04-28 19:39:36 +0300

    Update TodosContainer.js

--- containers/TodosContainer.js
+++ containers/TodosContainer.js
@@ -68,12 +68,7 @@ export default function TodosContainer({
           : styles.todos_container + " " + styles.darkmode
       }
     >
-      <ReactSortable
-        list={todoArray}
-        setList={setTodoArray}
-        delayOnTouchOnly={true}
-        delay={1}
-      >
+      <ReactSortable list={todoArray} setList={setTodoArray} delay={1000}>
         {displayList()}
       </ReactSortable>
 
