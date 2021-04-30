f3a0d872af572251c71d219a78ae996576ead424
p-alex <alexey9021@gmail.com>
2021-04-30 21:30:52 +0300

    Now todos are being saved in the localStorage

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,35 @@ import Header from "../components/Header";
 import TodosContainer from "../containers/TodosContainer";
 
 export default function Home() {
-  const [todoArray, setTodoArray] = useState([
-    { todo: "Complete online javascript course", isChecked: true, id: 0 },
-    { todo: "Jog around the park 3x", isChecked: false, id: 1 },
-    { todo: "10 minutes meditation", isChecked: false, id: 2 },
-    { todo: "Read for 1 hour", isChecked: false, id: 3 },
-    { todo: "Pick up groceries", isChecked: false, id: 4 },
-    { todo: "Complete Todo App on Frontend Mentor", isChecked: false, id: 5 },
-  ]);
-
+  const [todoArray, setTodoArray] = useState([]);
   const [input, setInput] = useState("");
   const [filter, setFilter] = useState("All");
   const [theme, setTheme] = useState("light");
+  const [toggleInitializeArray, setToggleInitializeArray] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("todoArray") === null)
+      localStorage.setItem(
+        "todoArray",
+        JSON.stringify([
+          { todo: "Complete online javascript course", isChecked: true, id: 0 },
+          { todo: "Jog around the park 3x", isChecked: false, id: 1 },
+          { todo: "10 minutes meditation", isChecked: false, id: 2 },
+          { todo: "Read for 1 hour", isChecked: false, id: 3 },
+          { todo: "Pick up groceries", isChecked: false, id: 4 },
+          {
+            todo: "Complete Todo App on Frontend Mentor",
+            isChecked: false,
+            id: 5,
+          },
+        ])
+      );
+    setTodoArray(JSON.parse(localStorage.getItem("todoArray")));
+  }, [toggleInitializeArray]);
+
+  useEffect(() => {
+    localStorage.setItem("todoArray", JSON.stringify(todoArray));
+  }, [todoArray]);
 
   useEffect(() => {
     document.body.style.backgroundColor =
@@ -28,26 +45,36 @@ export default function Home() {
   const handleAdd = (e) => {
     e.preventDefault();
     if (input) {
-      setTodoArray([
-        ...todoArray,
-        { todo: input, isChecked: false, id: todoArray.length },
-      ]);
+      let array = JSON.parse(localStorage.getItem("todoArray"));
+      let updatedArray = [
+        ...array,
+        { todo: input, id: array.length, isChecked: false },
+      ];
+      localStorage.setItem("todoArray", JSON.stringify(updatedArray));
       setInput("");
+      setToggleInitializeArray(!toggleInitializeArray);
     }
   };
-  const handleCheck = (itemId) => {
-    const updatedArray = todoArray.map((item) => {
-      if (itemId === item.id) {
-        item.isChecked = !item.isChecked;
-        return item;
+
+  const handleCheck = (itemID) => {
+    console.log(itemID);
+    let array = JSON.parse(localStorage.getItem("todoArray"));
+    let updatedArray = array.map((item) => {
+      if (itemID === item.id) {
+        return { ...item, isChecked: !item.isChecked };
       }
       return item;
     });
-    setTodoArray(updatedArray);
+    localStorage.setItem("todoArray", JSON.stringify(updatedArray));
+    setToggleInitializeArray(!toggleInitializeArray);
   };
 
-  const handleDelete = (itemId) =>
-    setTodoArray(todoArray.filter((item) => item.id !== itemId));
+  const handleDelete = (itemID) => {
+    let array = JSON.parse(localStorage.getItem("todoArray"));
+    let updatedArray = array.filter((item) => item.id !== itemID);
+    localStorage.setItem("todoArray", JSON.stringify(updatedArray));
+    setToggleInitializeArray(!toggleInitializeArray);
+  };
 
   const handleClearCompleted = () =>
     setTodoArray(todoArray.filter((item) => !item.isChecked));
